ff1fb8212fd4b7d8a08295183ab5cb1d77c3a34f
Alisha5555 <alisha.dixon15@ed.cna.nl.ca>
2026-03-17 16:45:10 -0230

    export setup

--- app/characters/[id]/page.jsx
+++ app/characters/[id]/page.jsx
@@ -1,6 +1,16 @@
 import Character from "@/components/Character.jsx"
+
+export async function generateStaticParams() {
+  const res = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await res.json();
+
+  return data.results.map((item) => ({
+    id: item.id.toString(),
+  }));
+}
+
 export default async function CharacterDetail( props ) {
-    const {id} = await props.params;
+    const {id} = props.params;
 
     const response = await fetch("https://rickandmortyapi.com/api/character/" + id )
     const data = await response.json()
